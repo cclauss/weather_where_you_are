6a0894f4f4a4ad4253b6b7cd154160461df9e63d
cclauss <cclauss@bluewin.ch>
2014-02-16 10:40:39 +0100

    Update weather_where_you_are.py

--- weather_where_you_are.py
+++ weather_where_you_are.py
@@ -2,11 +2,12 @@
 ''' Print out current weather at your current location. '''
 import location, requests, time
 def getLocation():
-location.start_updates()
-time.sleep(1)
-currLoc = location.get_location()
-location.stop_updates() # stop GPS hardware ASAP to save battery
-return currLoc
+    location.start_updates()
+    time.sleep(1)
+    currLoc = location.get_location()
+    location.stop_updates() # stop GPS hardware ASAP to save battery
+    return currLoc
+
 your_loc = location.reverse_geocode(getLocation())[-1]
 # import pprint ; pprint.pprint(your_loc) # useful for debugging
 # See: http://bugs.openweathermap.org/projects/api/wiki
@@ -16,18 +17,18 @@ the_url = base_url + url_fmt.format(**your_loc).replace(' ', '+')
 print(the_url)
 weather = requests.get(the_url).json()
 if weather:
-# import pprint ; pprint.pprint(weather) # useful for debugging
-for item in ('temp_min', 'temp_max'):
-if item not in weather['main']:
-weather['main'][item] = None # create values if they are not present
-if 'weather' not in weather: # weather is optional in weather!!
-weather['weather'] = [ {'description' : 'not available'} ]
-for item in ('sunrise', 'sunset'):
-weather['sys'][item] = time.ctime(weather['sys'][item]).split()[3] # just time, not date
-print('''Current weather at {name}, {sys[country]} is {weather[0][description]}.
-Temprature: {main[temp]}c ({main[temp_min]}c / {main[temp_max]}c) (min / max)
-Pressure: {main[pressure]} hPa
-Humidity: {main[humidity]}%
-Sunrise: {sys[sunrise]}
-Sunset: {sys[sunset]}
-Weather information provided by openweathermap.org'''.format(**weather))
+    # import pprint ; pprint.pprint(weather) # useful for debugging
+    for item in ('temp_min', 'temp_max'):
+        if item not in weather['main']:
+            weather['main'][item] = None # create values if they are not present
+    if 'weather' not in weather: # weather is optional in weather!!
+        weather['weather'] = [ {'description' : 'not available'} ]
+    for item in ('sunrise', 'sunset'):
+        weather['sys'][item] = time.ctime(weather['sys'][item]).split()[3] # just time, not date
+    print('''Current weather at {name}, {sys[country]} is {weather[0][description]}.
+        Temprature: {main[temp]}c ({main[temp_min]}c / {main[temp_max]}c) (min / max)
+        Pressure: {main[pressure]} hPa
+        Humidity: {main[humidity]}%
+        Sunrise: {sys[sunrise]}
+        Sunset: {sys[sunset]}
+        Weather information provided by openweathermap.org'''.format(**weather))
